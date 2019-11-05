720a8ac758077fd68b92a7a98e2e465899b71dc1
tri-mira <tri-mira@mail.ru>
2019-11-05 13:26:11 +0300

    Создали класс Point

---
+++ Snake/Snake/Point.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Snake
+{
+    public class Point
+    {
+        
+            public int x;
+            public int y;
+            public char sym;
+
+            public void Drow()
+
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+    
+    }
+}
