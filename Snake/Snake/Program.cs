db9b33a0c25e0326fcf71ff8e3eda7fee5d339bd
tri-mira <tri-mira@mail.ru>
2019-11-05 11:58:49 +0300

    Hi TRI MIRA

---
+++ Snake/Snake/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi TRI MIRA!");
+            Console.ReadLine();
+        }
+    }
+}
